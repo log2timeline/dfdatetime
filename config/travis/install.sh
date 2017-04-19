24a73e8f3adf8c73a5dec47b9dcce02a07569d98
Joachim Metz <joachim.metz@gmail.com>
2017-04-19 22:36:24 +0200

    Updated utility scripts

--- config/travis/install.sh
+++ config/travis/install.sh
@@ -4,12 +4,29 @@
 
 COVERALL_DEPENDENCIES="python-coverage python-coveralls python-docopt";
 
+PYTHON2_DEPENDENCIES="";
+
+PYTHON2_TEST_DEPENDENCIES="python-mock";
+
+PYTHON3_DEPENDENCIES="";
+
+PYTHON3_TEST_DEPENDENCIES="python3-mock";
+
 # Exit on error.
 set -e;
 
-if test `uname -s` = "Linux";
+if test `uname -s` = "Darwin";
+then
+	git clone https://github.com/log2timeline/l2tdevtools.git;
+
+	mv l2tdevtools ../;
+	mkdir dependencies;
+
+	PYTHONPATH=../l2tdevtools ../l2tdevtools/tools/update.py --download-directory=dependencies --preset=dfdatetime;
+
+elif test `uname -s` = "Linux";
 then
 	sudo add-apt-repository ppa:gift/dev -y;
 	sudo apt-get update -q;
-	sudo apt-get install -y ${COVERALL_DEPENDENCIES};
+	sudo apt-get install -y ${COVERALL_DEPENDENCIES} ${PYTHON2_DEPENDENCIES} ${PYTHON2_TEST_DEPENDENCIES} ${PYTHON3_DEPENDENCIES} ${PYTHON3_TEST_DEPENDENCIES};
 fi

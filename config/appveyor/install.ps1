6dff9180d7edcb14f8007acd27627af4acf349d6
Joachim Metz <joachim.metz@gmail.com>
2023-04-09 05:18:01 +0200

    Changes for CI tests

--- config/appveyor/install.ps1
+++ config/appveyor/install.ps1
@@ -3,8 +3,9 @@
 $Dependencies = ""
 $Dependencies = ${Dependencies} -split " "
 
-$Output = Invoke-Expression -Command "git clone https://github.com/log2timeline/l2tdevtools.git ..\l2tdevtools 2>&1" | %{ "$_" }
-Write-Host (${Output} | Out-String)
+# Use cmd here since Invoke-Expression will raise NativeCommandError and want
+# stdout and stderr to not buffer until command completion.
+cmd /c "git clone https://github.com/log2timeline/l2tdevtools.git ..\l2tdevtools 2>&1"
 
 If ($env:APPVEYOR_REPO_BRANCH -eq "main")
 {
@@ -18,6 +19,7 @@ New-Item -ItemType "directory" -Name "dependencies"
 
 $env:PYTHONPATH = "..\l2tdevtools"
 
-$Output = Invoke-Expression -Command "& '${env:PYTHON}\python.exe' ..\l2tdevtools\tools\update.py --download-directory dependencies --machine-type ${env:MACHINE_TYPE} --msi-targetdir ${env:PYTHON} --track ${env:L2TBINARIES_TRACK} ${Dependencies} 2>&1"
-Write-Host (${Output} | Out-String)
+# Use cmd here since Invoke-Expression will raise NativeCommandError and want
+# stdout and stderr to not buffer until command completion.
+cmd /c "& '${env:PYTHON}\python.exe' ..\l2tdevtools\tools\update.py --download-directory dependencies --machine-type ${env:MACHINE_TYPE} --msi-targetdir ${env:PYTHON} --track ${env:L2TBINARIES_TRACK} ${Dependencies} 2>&1"
 

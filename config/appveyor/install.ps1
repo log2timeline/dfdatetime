d7c758751d6ffae577d8fcf6a66991c669b33302
Joachim Metz <joachim.metz@gmail.com>
2023-04-09 06:02:33 +0200

    Changes for CI tests

--- config/appveyor/install.ps1
+++ config/appveyor/install.ps1
@@ -3,9 +3,8 @@
 $Dependencies = ""
 $Dependencies = ${Dependencies} -split " "
 
-# Use cmd here since Invoke-Expression will raise NativeCommandError and want
-# stdout and stderr to not buffer until command completion.
-cmd /c "git clone https://github.com/log2timeline/l2tdevtools.git ..\l2tdevtools 2>&1"
+$Output = Invoke-Expression -Command "git clone https://github.com/log2timeline/l2tdevtools.git ..\l2tdevtools 2>&1" | %{ "$_" }
+Write-Host (${Output} | Out-String)
 
 If ($env:APPVEYOR_REPO_BRANCH -eq "main")
 {
@@ -19,7 +18,6 @@ New-Item -ItemType "directory" -Name "dependencies"
 
 $env:PYTHONPATH = "..\l2tdevtools"
 
-# Use cmd here since Invoke-Expression will raise NativeCommandError and want
-# stdout and stderr to not buffer until command completion.
-cmd /c "& '${env:PYTHON}\python.exe' ..\l2tdevtools\tools\update.py --download-directory dependencies --machine-type ${env:MACHINE_TYPE} --msi-targetdir ${env:PYTHON} --track ${env:L2TBINARIES_TRACK} ${Dependencies} 2>&1"
+$Output = Invoke-Expression -Command "& '${env:PYTHON}\python.exe' ..\l2tdevtools\tools\update.py --download-directory dependencies --machine-type ${env:MACHINE_TYPE} --msi-targetdir ${env:PYTHON} --track ${env:L2TBINARIES_TRACK} ${Dependencies} 2>&1" | %{ "$_" }
+Write-Host (${Output} | Out-String)
 

b5865d97b961b7151ce535e508fbc59997158e06
Joachim Metz <joachim.metz@gmail.com>
2025-11-24 16:08:44 +0100

    Changes for release and deployment (#296)

--- dfdatetime/__init__.py
+++ dfdatetime/__init__.py
@@ -25,4 +25,4 @@ from dfdatetime import uuid_time
 from dfdatetime import webkit_time
 
 
-__version__ = '20251018'
+__version__ = '20251124'
--- tests/rfc2579_date_time.py
+++ tests/rfc2579_date_time.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Tests for the RFC2579 date-time implementation."""
 
